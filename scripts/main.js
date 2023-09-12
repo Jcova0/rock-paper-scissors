07c9b4627ea5d87aebdf0117333bc237a3e7c432
Jonathan Covarrubias <jcova012@gmail.com>
2023-09-11 18:03:49 -0700

    Add computer choices

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+
+//Get random computer choice
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        return "rock";
+    }
+    else if (randomNumber === 1) {
+        return "paper";
+    }
+    else if (randomNumber === 2) {
+        return "scissors";
+    }   
+    else {
+        return "Error";
+    }
+}
+
+console.log(getComputerChoice());
