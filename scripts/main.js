0ab72c9185a1fb53f7591e4d2b70f13c83dd6072
Jonathan Covarrubias <jcova012@gmail.com>
2023-10-19 17:30:25 -0700

    Change console logs to use DOM manipulation, add a reset button, and add a score counter.

--- scripts/main.js
+++ scripts/main.js
@@ -1,75 +1,52 @@
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const playerScoreDisplay = document.querySelector("#player-score");
+const computerScoreDisplay = document.querySelector("#computer-score");
+const resultDisplay = document.querySelector("#result");
+
+
+//Variables for player and computer scores
+let playerScore = 0;
+let computerScore = 0;
+
 
 //Randomly choose between rock, paper, or scissors
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber === 0) {
-        return "rock";
-    }
-    else if (randomNumber === 1) {
-        return "paper";
-    }
-    else if (randomNumber === 2) {
-        return "scissors";
-    }   
-    else {
-        return "Error";
-    }
+    const randomNumber = Math.floor(Math.random() * 3);
+    const choices = ["rock", "paper", "scissors"];
+    return choices[randomNumber];
 }
 
 //Choose between rock, paper, or scissors
-function getPlayerChoice() {
-    let playerChoice = prompt("Choose rock, paper, or scissors: ");
-    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
-        return playerChoice;
-    }
-    else {
-        return "Error";
-    }
-}
+rock.addEventListener("click", () => {playRound("rock", getComputerChoice())});
+paper.addEventListener("click", () => {playRound("paper", getComputerChoice())});
+scissors.addEventListener("click", () => {playRound("scissors", getComputerChoice())});
 
 //Decide who wins the round
 function playRound(playerSelection, computerSelection) {
     let win = "You win!";
     let lose = "You lose!";
     let tie = "It's a tie!";
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-        return win;
-    }
-    else if (playerSelection === "paper" && computerSelection === "rock") {
-        return win;
-    }
-    else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return win;
+    if (playerSelection === "rock" && computerSelection === "scissors" ||
+    playerSelection === "paper" && computerSelection === "rock" ||
+    playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
     }
     else if (playerSelection === computerSelection) {
         return tie;
     }
     else {
-        return lose;
+        computerScore++;
+        computerScoreDisplay.textContent = computerScore;
     }
-}
-
-//Keep score of the game and determine who wins
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let tieScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
-        const roundResult = playRound(playerSelection, computerSelection);
-        if (roundResult === "You win!") {
-            playerScore++;
-        }
-        else if (roundResult === "You lose!") {
-            computerScore++;
-        }
-        else {
-            tieScore++;
-        }
+    if (playerScore === 5) {
+        resultDisplay.textContent = "You win!";
+        return "You win the game!";
     }
-
-    return `Player: ${playerScore} \nComputer: ${computerScore} \nTies: ${tieScore}`;
-}
-
-console.log(game());
+    else if (computerScore === 5) {
+        resultDisplay.textContent = "You lose!";
+        return "You lose the game!";
+    }
+};
